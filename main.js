868c4bfed61ed6ba4557985a6e99063bd08b5eb9
Md Tanvir Alam <tanviralamshuvo@gmail.com>
2017-04-30 17:38:44 +0600

    second

---
+++ main.js
@@ -0,0 +1,43 @@
+// var Node;
+// var Edge;
+
+
+// var MyIndustry = [];
+
+
+// var myInput = document.getElementById('myInput').value;
+
+//  var Node = function(){
+// 	this.node = [];
+// 	this.edge = {};
+// }
+
+
+
+// Node.prototype.addNode = function(m){
+
+// 	this.node.push(m);
+// }
+
+
+// var n = new Node;
+
+
+// function add(event){
+// 	console.log(event.screenX);
+// }
+
+// console.log(n);
+
+
+// console.log(n.node);
+// console.log(n.node[0]);
+
+
+
+function show(){
+	var s = document.getElementById("myInput").value;
+	var t = document.getElementById("demo");
+	t.innerHTML = s;
+	//console.log(s);
+}
